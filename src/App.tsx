f2a9ff8b63d7fde056fbe792972fcfe1b3d3c276
jcaicedo <jose.caicedo@outlook.com>
2022-09-07 15:56:23 -0500

    [ADD] route-navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header"
+import AppRoute from "@/routes"
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <AppRoute />
     </div>
   )
 }
---
+++ src/layouts/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home () {
+  return (
+    <>
+      <h1>inicio</h1>
+    </>
+  )
+}
+
+export default Home
