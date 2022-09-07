a250891fac86a2522f2c626378ca5c5891f588c3
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-09-07 17:04:41 -0500

    agregado config eslint y prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import Header from "@/components/header"
-import AppRoute from "@/routes"
-
+import Header from '@/components/header';
+import AppRoute from '@/routes';
 
 function App() {
   return (
     <div className="App">
       <AppRoute />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
