9b624552a68899b3ded9c9e5071cdc6ae2896b83
jcaicedo <jose.caicedo@outlook.com>
2022-09-08 16:48:40 -0500

    feat(add) card para favorito

--- src/layouts/Favorites.tsx
+++ src/layouts/Favorites.tsx
@@ -1,10 +1,12 @@
-import Card from '../components/card/index'
+// import Card from '@/components/card/index'
+import CardFavorite from '@/components/cardFavorite'
 
 const Favorites = (): JSX.Element => {
   return (
     <>
       <h1>Favorites</h1>
-      <Card></Card>
+{/*       <Card></Card> */}
+      <CardFavorite></CardFavorite>
     </>
   )
 }
