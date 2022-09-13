5ca785957a6640efd8621d471ea95e17487c4515
MaxC0d3 <maximitianoreste@gmail.com>
2022-09-12 19:16:17 -0500

    Fix(footer): Correccion estructura del link

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,15 +1,18 @@
 
 import Github from '@/assets/github.svg'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import './style.scss'
 
 const Footer = (): JSX.Element => {
   return (
     <div className="footer">
       <h1 className='logo'>MOVIE APP</h1>
-      <div className='repo' title='Go to repository'>
-        <a href="https://github.com/Open-Bootcamp/movie-app" target='_blank'>Github repository</a>
-        <img src={Github} alt="Github icon" />
-      </div>
+      <a href="https://github.com/Open-Bootcamp/movie-app" target='_blank'>
+        <div className='repo' title='Go to repository'>
+          <span className='txt_a'>Github repository</span>
+          <LazyLoadImage src={Github} alt="Github icon" />
+        </div>
+      </a>
       <span className='chl'>Copyright 2022. All Rights Reserved.</span>
     </div>
   )
